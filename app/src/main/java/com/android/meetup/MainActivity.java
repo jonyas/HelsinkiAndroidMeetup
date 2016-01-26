ae0f2dabc197faa2af5c755aecdb1aa50c9133cb
Jon Duran <jon.duran@zalando.fi>
2016-01-26 17:00:38 +0200

    Simplified json

--- app/src/main/java/com/android/meetup/MainActivity.java
+++ app/src/main/java/com/android/meetup/MainActivity.java
@@ -11,10 +11,6 @@ import com.android.meetup.model.WeatherData;
 public class MainActivity extends AppCompatActivity {
 
     private final String jsonData = "{\n" +
-            "  \"coord\": {\n" +
-            "    \"lon\": 24.97,\n" +
-            "    \"lat\": 60.17\n" +
-            "  },\n" +
             "  \"weather\": [\n" +
             "    {\n" +
             "      \"id\": 801,\n" +
@@ -23,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             "      \"icon\": \"02d\"\n" +
             "    }\n" +
             "  ],\n" +
-            "  \"base\": \"cmc stations\",\n" +
             "  \"main\": {\n" +
             "    \"temp\": 251.79,\n" +
             "    \"pressure\": 1029,\n" +
@@ -31,25 +26,7 @@ public class MainActivity extends AppCompatActivity {
             "    \"temp_min\": 249.15,\n" +
             "    \"temp_max\": 253.15\n" +
             "  },\n" +
-            "  \"wind\": {\n" +
-            "    \"speed\": 1,\n" +
-            "    \"deg\": 330\n" +
-            "  },\n" +
-            "  \"clouds\": {\n" +
-            "    \"all\": 20\n" +
-            "  },\n" +
-            "  \"dt\": 1453458618,\n" +
-            "  \"sys\": {\n" +
-            "    \"type\": 1,\n" +
-            "    \"id\": 5018,\n" +
-            "    \"message\": 0.0105,\n" +
-            "    \"country\": \"FI\",\n" +
-            "    \"sunrise\": 1453445823,\n" +
-            "    \"sunset\": 1453471616\n" +
-            "  },\n" +
-            "  \"id\": 653708,\n" +
-            "  \"name\": \"Katajanokka\",\n" +
-            "  \"cod\": 200\n" +
+            "  \"name\": \"Katajanokka\"\n" +
             "}\n";
 
     private WeatherData weatherData;
